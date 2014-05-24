f1ff5b806a0b56c4f71e162ae46f13b18f479c80
Marc DellaVolpe <marc.dellavolpe@gmail.com>
2014-05-24 03:20:41 -0400

    touch

--- src/github.com/mdellavo/logger/logger.go
+++ src/github.com/mdellavo/logger/logger.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"flag"
 	"strings"
 )
 
@@ -13,21 +14,26 @@ func write(conn net.Conn, s string) {
 	conn.Write([]byte(s))
 }
 
-func main() {
+var host = flag.String("h", "localhost", "host to log to")
 
-	conn, err := net.Dial("udp", "quuux.org:5222")
+func main() {
+	flag.Parse()
+
+	conn, err := net.Dial("udp", *host + ":5222")
 	defer conn.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if len(os.Args) > 1 && os.Args[1] == "-" {
+	args := flag.Args()
+
+	if len(args) > 0 && args[0] == "-" {
 		scanner := bufio.NewScanner(os.Stdin)
 		for scanner.Scan() {
 			write(conn, scanner.Text())
 		}
 	} else {
-		write(conn, strings.Join(os.Args[1:], " "))
+		write(conn, strings.Join(args, " "))
 	}
 
 }
